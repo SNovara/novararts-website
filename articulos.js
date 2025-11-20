69ef100829b9852a2a8b70c8e109502608b32686
SNovara <santinovara@hotmail.com>
2025-11-20 15:21:01 -0300

    Cambio de mayúsculas y tildes
    Se cambiaron mayúsculas y tildes de las rutas

--- articulos.js
+++ articulos.js
@@ -5,8 +5,8 @@ const articulos = [
     titulo: "La crisis oculta de Nintendo",
     descripcion: "Una verdad escondida detrás de sus éxitos más recientes.",
     fecha: "2025-11-10",
-    url: "Artículos/articulo1.html",
+    url: "articles/articulo1.html",
     tag: "Opinión",
-    thumb: "Imágenes/Thumbnail_Images/Nintendo_gray_logo.jpg",
+    thumb: "images/thumbnail-images/Nintendo_gray_logo.jpg",
   },
 ];
