c30b8859e07a3917e0e4091ee91aafe489a06a0a
SNovara <santinovara@hotmail.com>
2025-11-23 15:31:40 -0300

    Nuevo artículo

--- articulos.js
+++ articulos.js
@@ -2,11 +2,19 @@
 
 const articulos = [
   {
-    titulo: "La crisis oculta de Nintendo",
-    descripcion: "Una verdad escondida detrás de sus éxitos más recientes.",
-    fecha: "2025-11-10",
-    url: "articles/articulo1.html",
+    titulo: "10 juegos que faltan en Nintendo Switch",
+    descripcion: "Un listado de juegos que quisiera ver en la híbrida",
+    fecha: "2025-11-23",
+    url: "articles/10-juegos-que-faltan-en-switch.html",
     tag: "Opinión",
     thumb: "images/thumbnail-images/Nintendo-Switch.jpg",
+  },
+  {
+    titulo: "La crisis oculta de Nintendo", /* ----- Publicado el 20/11/2025 ----- */
+    descripcion: "Una verdad escondida detrás de sus éxitos más recientes.",
+    fecha: "2025-11-20",
+    url: "articles/articulo1.html",
+    tag: "Opinión",
+    thumb: "images/thumbnail-images/Nintendo-Switch-2.png",
   },
 ];
