1e24cceef7b6f3c9a48b4ae25a1d24ec39480d06
SNovara <santinovara@hotmail.com>
2025-12-24 16:40:56 -0300

    Rework de sección de portfolio

---
+++ projects/Ducklas/ducklas.js
@@ -0,0 +1,11 @@
+// ==== Base de proyectos ====
+
+const articulos = [
+  {
+    titulo: "Ducklas – Prueba",
+    descripcion: "Prueba para Ducklas",
+    fecha: "2025-12-05",
+    url: "ducklas-prueba.html",
+    thumb: "../../images/thumbnail-images/Music-Note.png",
+  }
+];
