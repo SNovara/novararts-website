19b0e42ee53952f59310c86d1ce1a1c0c865e6da
SNovara <santinovara@hotmail.com>
2025-12-27 06:06:09 -0300

    Ajustes estilísticos y de nomenclatura

--- articulos.js
+++ articulos.js
@@ -45,7 +45,7 @@ const articulos = [
     titulo: "10 juegos que faltan en Nintendo Switch",
     descripcion: "Un listado de juegos que quisiera ver en la híbrida.",
     fecha: "2025-11-23",
-    url: "articles/10-juegos-que-faltan-en-switch_v2.html",
+    url: "articles/10-juegos-que-faltan-en-switch.html",
     tag: "Opinión",
     thumb: "images/thumbnail-images/Nintendo-Switch.jpg",
   },
