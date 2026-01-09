e9d5a6ca1d2dbead2fa1c820fdcb5d9f35cf5143
SNovara <santinovara@hotmail.com>
2026-01-09 15:39:54 -0300

    Corrección semántica y de fecha

--- projects/Ducklas/ducklas.js
+++ projects/Ducklas/ducklas.js
@@ -15,9 +15,10 @@ const articulos = [
     url: "ducklas-gdd.html",
     thumb: "/images/Ducklas-GDD.png",
   },
+  {
     titulo: "DUCKLAS: el guion",
     descripcion: "La prueba piloto para TV.",
-    // fecha: "2025-12-30",
+    // fecha: "2026-09-01",
     url: "ducklas-el-guion.html",
     thumb: "/images/Ducklas-Script.png",
   },
